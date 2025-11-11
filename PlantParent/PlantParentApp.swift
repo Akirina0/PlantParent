f1568f2586816cf2040548d84de7f7886b114315
GiuliaG <giulia.guadagno@studenti.unisob.na.it>
2025-11-11 10:36:15 +0100

    Initial Commit

---
+++ PlantParent/PlantParentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantParentApp.swift
+//  PlantParent
+//
+//  Created by Giulia Guadagno on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PlantParentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
